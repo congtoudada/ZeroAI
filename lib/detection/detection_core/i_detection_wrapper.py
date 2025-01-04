1794b53db30d8ad663c34772de7359851930c6b9
聪头的游戏世界 <1322080797@qq.com>
2025-01-04 17:02:11 +0800

    目标检测重构

--- lib/detection/detection_core/i_detection_wrapper.py
+++ lib/detection/detection_core/i_detection_wrapper.py
@@ -3,9 +3,11 @@ from abc import ABC, abstractmethod
 
 class IDetectionWrapper(ABC):
     @abstractmethod
-    def inference(self, img):
+    def inference(self, frame):
         """
-        输入图像，返回检测结果，格式要求
+        输入图像，返回检测结果
+        :param frame: 输入图像帧(ndarray格式)
+        :return:
         # ndarray shape: [n, 6]
         # n: n个对象
         # [0,1,2,3]: ltrb bboxes (基于img分辨率)
@@ -15,8 +17,6 @@ class IDetectionWrapper(ABC):
         #   [3]: y2
         # [4]: 置信度
         # [5]: 类别 (下标从0开始)
-
-        :param img: 输入图像(ndarray格式)
         """
         pass
 
