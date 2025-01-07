1884d2259ec4607d4114cf3102066b5858aa6fc2
聪头的游戏世界 <1322080797@qq.com>
2025-01-07 16:11:44 +0800

    人脸帮助类规范化，改进ObjectPool

--- lib/facr/insight/zero/component/face_helper.py
+++ lib/facr/insight/zero/component/face_helper.py
@@ -8,6 +8,7 @@ from insight.zero.component.insight_comp import InsightComponent
 from insight.zero.info.face_helper_info import FaceHelperInfo
 from utility.config_kit import ConfigKit
 from utility.img_kit import ImgKit
+from utility.object_pool import ObjectPool
 from zero.core.global_constant import GlobalConstant
 
 
@@ -23,9 +24,10 @@ class FaceHelper:
         self.pname = f"[ {os.getpid()}:face_helper ]"
         self.face_shared_memory = UltraDict(name=InsightComponent.SHARED_MEMORY_NAME, shared_lock=GlobalConstant.LOCK_MODE)
         self.handler = FaceProcessHelper(self.face_shared_memory, self.config, self.face_callback)
+        self.dict_pool = ObjectPool(20, dict)
         # key: obj_id
-        # value: { "per_id": 1, "score": 0 }
-        self.face_dict: Dict[int, dict] = {}  # 人脸识别结果集
+        # value: { "last_time": 0, "retry": 0, "per_id": 1, "score": 0 }
+        self.face_dict: Dict[int, dict] = {}
         self.callback = callback
 
     def tick(self, now):
@@ -43,37 +45,46 @@ class FaceHelper:
         for key in clear_keys:
             self.face_dict.pop(key)  # 从字典中移除item
 
-    def try_send(self, now, frame, ltrb, obj_id, diff, per_y, retry) -> bool:
+    def try_send(self, now, frame, ltrb, obj_id, obj_y=-1, cam_id=0) -> bool:
         """
         尝试发送识别请求
+        :param now: 当前时间
+        :param frame: 当前帧图像
+        :param ltrb: 包围框
+        :param obj_id: 对象id
+        :param obj_y: 对象y轴百分比，用于限定有效识别区域
+        :param cam_id: 请求摄像头id (仅调试)
         :return:
         """
-        # 保温
-        if self.face_dict.__contains__(obj_id):
-            self.face_dict[obj_id].update({
-                "last_time": now
-            })
+        # 新建
+        if not self.face_dict.__contains__(obj_id):
+            face_item: dict = self.dict_pool.pop()
+            # face_item.clear()  # 手动赋值
+            face_item["last_time"] = now
+            face_item["retry"] = 0
+            face_item["per_id"] = 1
+            face_item["score"] = 0
+            self.face_dict[obj_id] = face_item
+            req_diff = now
+        else:  # 保温
+            req_diff = now - self.face_dict[obj_id]["last_time"]
+            self.face_dict[obj_id]["last_time"] = now
 
+        retry = self.face_dict[obj_id]["retry"]
         if retry > self.config.face_max_retry:  # 大于重试次数，不发送
             return False
-        if self.face_dict.__contains__(obj_id) and self.face_dict[obj_id]['per_id'] != 1:  # 不是陌生人，不发送
+        per_id = self.face_dict[obj_id]["per_id"]
+        if per_id != 1:  # 不是陌生人，不发送
             return False
-        if not diff > self.config.face_min_send_interval:  # 不满足发送间隔，不发送
+        if req_diff < self.config.face_min_send_interval:  # 小于发送间隔，不发送
             return False
-        if not self.config.face_cull_up_y < per_y < 1.0 - self.config.face_cull_down_y:  # 在剔除区域，不发送
+        if obj_y != -1 and self.config.face_cull_up_y < obj_y < 1.0 - self.config.face_cull_down_y:  # 在剔除区域，不发送
             return False
         # 尝试发送人脸识别请求（内部可能还会判断）
         if frame is not None:
             image = ImgKit.crop_img(frame, ltrb)
             if image is not None:
-                ret = self.handler.send(obj_id, image)
-                if ret:
-                    self.face_dict[obj_id] = {
-                        "last_time": now,
-                        "per_id": 1,
-                        "score": 0
-                    }
-                    return True
+                return self.handler.send(obj_id, image, cam_id)
         return False
 
     def destroy_obj(self, obj_id):
@@ -89,10 +100,8 @@ class FaceHelper:
         logger.info(f"{self.pname} 收到人脸响应: {obj_id} {per_id} {score}")
         if self.face_dict.__contains__(obj_id):
             # 添加到结果集缓存
-            self.face_dict[obj_id].update({
-                "per_id": per_id,
-                "score": score
-            })
+            self.face_dict[obj_id]["per_id"] = per_id
+            self.face_dict[obj_id]["score"] = score
             # 触发外界回调函数
             if self.callback is not None:
                 self.callback(obj_id, per_id, score)
