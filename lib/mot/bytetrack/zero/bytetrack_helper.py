deaafeb9212e5ef05f1c341bdde252bc7f672290
聪头的游戏世界 <1322080797@qq.com>
2025-03-01 00:31:55 +0800

    mot支持reid存图

--- lib/mot/bytetrack/zero/bytetrack_helper.py
+++ lib/mot/bytetrack/zero/bytetrack_helper.py
@@ -5,21 +5,77 @@ import numpy as np
 
 from bytetrack.zero.bytetrack_info import BytetrackInfo
 from bytetrack.zero.byte_tracker import BYTETracker, STrack
+from reid_core.helper.reid_helper import ReidHelper
 from utility.config_kit import ConfigKit
+from utility.img_kit import ImgKit
 
 
 class BytetrackHelper:
     def __init__(self, config_path: str):
         self.config: BytetrackInfo = BytetrackInfo(ConfigKit.load(config_path))
-        self.pname = f"[ {os.getpid()}:bytetrack ]"
+        self.pid = os.getpid()
+        self.pname = f"[ {self.pid}:bytetrack ]"
         # byetrack 模型
         self.tracker = BYTETracker(self.config, frame_rate=self.config.bytetrack_args_fps)
+        # reid存图缓存
+        # key: obj_id
+        # value: {count: 0, last_time: 0, last_save_time: 0}  存图次数,上次更新时间,上次存图时间
+        self.reid_cache = {}
 
-    def inference(self, input_det):
+    def inference(self, input_det, now=0, frame=None, cam_id=1):
         if input_det is None:
             return None
         else:
-            return self.alignment_result(self.tracker.update(input_det))
+            result = self.alignment_result(self.tracker.update(input_det))
+            # reid存图
+            self.save_reid_extension(now, result, frame, cam_id)
+            return result
+
+    def save_reid_extension(self, now, mot_result, frame, cam_id=1):
+        if not self.config.bytetrack_reid_enable:
+            return
+        if mot_result is None:
+            return
+        if frame is None:
+            return
+        # 清除无用缓存
+        for k, v in self.reid_cache.items():
+            if now - v['last_time'] > self.config.bytetrack_reid_lost_frames:
+                self.reid_cache.pop(k)
+        # 遍历-存图
+        for obj in mot_result:
+            cls = int(obj[5])  # 提取当前目标的类别，转换为整数类型
+            # 不是人不存(第0类)
+            if cls != 0:
+                continue
+            obj_id = int(obj[6])  # 提取当前目标的唯一标识符，转换为整数类型
+            cache_item = None
+            if not self.reid_cache.__contains__(obj_id):
+                cache_item = {'count': 0, 'last_time': now, 'last_save_time': 0}
+                self.reid_cache[obj_id] = cache_item
+            else:
+                # 发送间隔不满足的跳过
+                cache_item = self.reid_cache[obj_id]
+                cache_item['last_time'] = now  # 保持对象活性
+                valid_interval = min(self.config.bytetrack_reid_max_interval,
+                                     self.config.bytetrack_reid_min_interval * cache_item['count'])
+                if now - cache_item['last_save_time'] < valid_interval:
+                    continue
+            conf = obj[4]  # 提取当前目标的置信度
+            # 置信度低的不存
+            if conf < self.config.bytetrack_reid_conf:
+                continue
+            ltrb = obj[:4]  # 提取当前目标的边界框坐标，即左上角和右下角的坐标
+            # 包围盒太小的不存或比例奇怪的不存
+            w = ltrb[2] - ltrb[0]
+            h = ltrb[3] - ltrb[1]
+            if w < 40 or h < 80:
+                continue
+            shot_img = ImgKit.crop_img(frame, ltrb)
+            if shot_img is not None:
+                ReidHelper.send_save_timing(cam_id, self.pid, obj_id, shot_img)
+                cache_item['count'] += 1
+                cache_item['last_save_time'] = now
 
     def alignment_result(self, online_targets: List[STrack]):
         """
--- lib/mot/bytetrack/zero/bytetrack_info.py
+++ lib/mot/bytetrack/zero/bytetrack_info.py
@@ -11,6 +11,13 @@ class BytetrackInfo(BaseInfo):
         self.bytetrack_args_min_box_area = 5
         self.bytetrack_args_mot20 = False
         self.detection_labels = []
+        # reid存图支持
+        self.bytetrack_reid_enable = False  # 是否支持reid存图(若支持，会定期对第0个类别发送reid存图请求)
+        self.bytetrack_reid_conf = 0.7  # reid存图阈值
+        self.bytetrack_reid_min_interval = 30  # reid存图最小间隔(支持reid存图才有效)
+        self.bytetrack_reid_max_interval = 300  # reid存图最大间隔(支持reid存图才有效)
+        self.bytetrack_reid_lost_frames = 180  # 对象消失多少帧则销毁
+        self.bytetrack_reid_camera_gallery = "output/service/clip_reid/camera_gallery"  # 存图路径(支持reid存图才有效)
         super().__init__(data)  # 前面是声明，一定要最后调用这段赋值
 
 
